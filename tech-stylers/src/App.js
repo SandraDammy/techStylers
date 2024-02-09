eef37264cab9158230defed207dabdbae1e08ecb
OluwadamilolaOd <oluwadamilolaod@ha-shem.com>
2024-02-09 14:03:20 +0100

    update

--- tech-stylers/src/App.js
+++ tech-stylers/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Components/Common/Navbar.jsx'
+import Footer from './Components/Common/Footer.jsx'
+import AboutUs from './Components/Page/AboutUs.jsx'
+import Blog from './Components/Page/Blog.jsx'
+import Contact from './Components/Page/Contact.jsx'
+import Event from './Components/Page/Event.jsx'
+import Gallery from './Components/Page/Event.jsx'
+import Resources from './Components/Page/Resources.jsx'
+import Home from './Components/Page/Home.jsx'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <div>
+        <Navbar/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={AboutUs} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/event" component={Event} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/resources" component={Resources} />
+
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
     </div>
   );
 }
