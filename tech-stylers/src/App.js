84f52fc22c2da69973ee4a40464515ff9da2be09
OluwadamilolaOd <oluwadamilolaod@ha-shem.com>
2024-02-09 15:31:16 +0100

    homePage

--- tech-stylers/src/App.js
+++ tech-stylers/src/App.js
@@ -1,34 +1,32 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './Components/Common/Navbar.jsx'
-import Footer from './Components/Common/Footer.jsx'
-import AboutUs from './Components/Page/AboutUs.jsx'
-import Blog from './Components/Page/Blog.jsx'
-import Contact from './Components/Page/Contact.jsx'
-import Event from './Components/Page/Event.jsx'
-import Gallery from './Components/Page/Event.jsx'
-import Resources from './Components/Page/Resources.jsx'
-import Home from './Components/Page/Home.jsx'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Common/Navbar.jsx";
+import Footer from "./Components/Common/Footer.jsx";
+import AboutUs from "./Components/Page/AboutUs.jsx";
+import Blog from "./Components/Page/Blog.jsx";
+import Contact from "./Components/Page/Contact.jsx";
+import Event from "./Components/Page/Event.jsx";
+import Gallery from "./Components/Page/Gallery.jsx";
+import Resources from "./Components/Page/Resources.jsx";
+import Home from "./Components/Page/Home.jsx";
 
 function App() {
   return (
     <div>
-      <Router>
-      <div>
-        <Navbar/>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={AboutUs} />
-          <Route path="/blog" component={Blog} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/event" component={Event} />
-          <Route path="/gallery" component={Gallery} />
-          <Route path="/resources" component={Resources} />
-
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/event" element={<Event />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/resources" element={<Resources />} />
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ tech-stylers/src/Components/Section/LandingPage.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <section>
+      <div>
+        <div>
+          <div>
+          <h1>
+            Empowering <span>Women</span>In <span>Tech</span>
+          </h1>
+          <p>Breaking Barriers, Building Futures.</p>
+          </div>
+          <div>
+            <button>Join Now</button>
+          </div>
+        </div>
+        <div>
+            <div>
+                <p className="big">520+</p>
+                <p className="txt">Members</p>
+            </div>
+            <div>
+                <p className="big">15+</p>
+                <p className="txt">Mentors</p>
+            </div>
+            <div>
+                <p className="big">5</p>
+                <p className="txt">Workshops</p>
+            </div>
+            <div>
+                <p className="big">6</p>
+                <p className="txt">Workshops</p>
+            </div>
+            <div>
+                <p className="big">1</p>
+                <p className="txt">Conference</p>
+            </div>
+        </div>
+      </div>
+      <div>
+        <img />
+      </div>
+    </section>
+  );
+};
+
+export default LandingPage;
