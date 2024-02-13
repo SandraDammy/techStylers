6662c9d1cc1ec3c1dcaf08f2a0b0c51a670dd252
OluwadamilolaOd <oluwadamilolaod@ha-shem.com>
2024-02-13 17:14:48 +0100

    Gallery

--- src/Components/Page/Gallery.jsx
+++ src/Components/Page/Gallery.jsx
@@ -1,9 +1,162 @@
-import React from 'react'
+import React from "react";
+import style from "../Style/Resources.module.css";
+import imgResources from "../../Assets/Image/Resources Image.png";
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
-  )
-}
+    <div className={style.container}>
+      <div className={style.eventContainer}>
+        <div className={style.txtLink}>
+          <div>
+            <p>TechStyler Event 2021</p>
+          </div>
+          <div>
+            <a href="#" className={style.seeLink}>
+              See All
+            </a>
+          </div>
+        </div>
+        <div className={style.cardGrid}>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+        </div>
+      </div>
+      <div className={style.eventContainer}>
+      <div className={style.txtLink}>
+          <div>
+            <p>TechStyler Event 2022</p>
+          </div>
+          <div>
+            <a href="#" className={style.seeLink}>
+              See All
+            </a>
+          </div>
+        </div>
+        <div className={style.cardGrid}>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+        </div>
+      </div>
+      <div className={style.eventContainer}>
+      <div className={style.txtLink}>
+          <div>
+            <p>TechStyler Event 2023</p>
+          </div>
+          <div>
+            <a href="#" className={style.seeLink}>
+              See All
+            </a>
+          </div>
+        </div>
+        <div className={style.cardGrid}>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+          <div className={style.card}>
+            <img src={imgResources} alt="Resources Img" className={style.img} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Gallery
+export default Gallery;
