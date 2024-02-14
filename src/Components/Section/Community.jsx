526406201a680484043696dee85d2e1142974f24
OluwadamilolaOd <oluwadamilolaod@ha-shem.com>
2024-02-14 14:28:02 +0100

    designUpdate

--- src/Components/Section/Community.jsx
+++ src/Components/Section/Community.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import style from "../Style/Community.module.css";
-import Title from "../Common/Title";
 
 const Community = () => {
   return (
@@ -11,10 +10,7 @@ const Community = () => {
       </div>
       <div className={style.textContainer}>
         <h1>A Community for Tech-Savy Women </h1>
-        {/* <Title
-          title={"A Community for Tech-Savy Women"}
-          className={"text40"}
-        /> */}
+       
         <p>
           This community brings women together, to learn, and grow in the tech
           industry. We offer workshops, events, and resources to help women
