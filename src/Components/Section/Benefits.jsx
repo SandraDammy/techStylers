8fd47a9d9f00da8810eb6a9299207a4cfb203bed
Odumuyiwa Teslim <teslimodumuyiwa@gmail.com>
2024-02-28 16:09:09 +0100

    [Feat]: Include motion to Benefits and Community section

--- src/Components/Section/Benefits.jsx
+++ src/Components/Section/Benefits.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import style from "../Style/Benefits.module.css";
 import benefits from "../../Assets/Image/Benefits.png";
+import { useState, useEffect, useRef } from "react";
 
 const Benefits = () => {
+  const [isActive, setIsActive] = useState(false)
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const current = containerRef.current
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setIsActive(true);
+        }
+      },
+      { threshold: 0.8 } // Optional: Observe when 80% of the element is visible
+    );
+
+    if (current) {
+      observer.observe(current);
+    }
+
+    return () => {
+      if (current) {
+        observer.unobserve(current);
+      }
+    };
+  }, []);
+  
   return (
-    <div className={style.container}>
+    <div className={style.container} ref={containerRef}>
       <div className={style.title}>
         <div className={style.hr}></div>
         <p>Benefits</p>
@@ -31,7 +57,7 @@ const Benefits = () => {
           </div>
         </div>
         <div className={style.imgContainer}>
-          <img src={benefits} alt="" />
+          <img src={benefits} alt="" className={isActive ? style.active : ''} />
         </div>
       </div>
     </div>
